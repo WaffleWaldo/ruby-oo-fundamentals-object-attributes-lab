48448e92da52d0b07355b9237070f3ee204fa2db
WaffleWaldo <oswaldoortiz0519@gmail.com>
2020-09-15 10:59:05 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+    attr_reader :name
+    attr_writer :name
+
+    attr_reader :breed
+    attr_writer :breed
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+    attr_reader :name
+    attr_writer :name
+
+    attr_reader :job
+    attr_writer :job
+end
